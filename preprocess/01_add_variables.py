5cc018ced05217c27eff13d8cfbdc14cc14c7ef2
juan antonio <juan_ryes.94@hotmail.com>
2025-05-05 14:22:42 -0600

    script limpieza de datos

---
+++ preprocess/01_add_variables.py
@@ -0,0 +1,4 @@
+import os, sys
+sys.path.append(os.getcwd()) # Esto es para agregar al path la ruta de ejecución actual y poder importar respecto a la ruta del proyecto, desde donde se debe ejecutar el código
+
+import pandas as pd
